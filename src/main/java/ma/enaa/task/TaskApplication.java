bebc1d3a6bae373b65cb6cd8a9bccb241a679efd
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-05-08 17:01:08 +0100

    first commit

---
+++ src/main/java/ma/enaa/task/TaskApplication.java
@@ -0,0 +1,13 @@
+package ma.enaa.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+}
