eda6c859c5eba15fb5e768c17a42c9e2fb148ba1
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-05-08 17:55:14 +0100

    modification

--- src/main/java/ma/enaa/task/TaskApplication.java
+++ src/main/java/ma/enaa/task/TaskApplication.java
@@ -1,13 +1,29 @@
 package ma.enaa.task;
 
+import ma.enaa.task.entities.Product;
+import ma.enaa.task.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class TaskApplication {
+public class TaskApplication implements CommandLineRunner {
+    @Autowired
+    private ProductRepository productRepository;
 
     public static void main(String[] args) {
+
         SpringApplication.run(TaskApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.save(new Product("idriss","informatique",99.1,new Date()));
+        productRepository.save(new Product("Ali","pc",111.3,new Date()));
+        productRepository.save(new Product("ahemd","SVT",919.1,new Date()));
+
+    }
 }
--- src/main/java/ma/enaa/task/entities/Product.java
+++ src/main/java/ma/enaa/task/entities/Product.java
@@ -1,9 +1,6 @@
 package ma.enaa.task.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +12,60 @@ import java.util.Date;
 public class Product {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+    @Column(length = 50)
     String name;
     String category;
     Double price;
+    @Temporal(TemporalType.DATE)
     Date date;
 
+    public Product(String name, String category, Double price, Date date) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.date = date;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
